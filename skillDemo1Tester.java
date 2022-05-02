bccb1b6fab372fc92349340be91870cdbbf923e6
Rena Hu <r3hu@ucsd.edu>
2022-05-01 17:34:16 -0700

    Added multiply method and tester file

---
+++ skillDemo1Tester.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
+import org.junit.*;
+
+public class skillDemo1Tester {
+    @Test 
+    public void testmultiply() {
+        assertEquals("should return 4 * 5 = 20", 9, 
+            skillDemo1.multiply(4,5));
+    }
+}
